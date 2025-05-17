697a5fae17395fe028dec85a75c80f7737ae669c
tobennaUmeh <94992053+tobennaUmeh@users.noreply.github.com>
2025-05-17 18:52:43 +0100

    Add TabLayout component with navigation tabs for Home and About

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,17 @@
+import { Tabs } from 'expo-router';
+import React from 'react';
+
+export default function TabLayout() {
+	return (
+		<Tabs>
+			<Tabs.Screen
+				name='index'
+				options={{ title: 'Home' }}
+			/>
+			<Tabs.Screen
+				name='about'
+				options={{ title: 'About' }}
+			/>
+		</Tabs>
+	);
+}
